5fad038ff5547b23e95057c33f617bf4aca5529a
santiagohernandezalicia <nolooolvides@gmail>
2025-01-24 10:57:39 -0700

    "rango"

--- linea/app.py
+++ linea/app.py
@@ -1,11 +1,14 @@
 # calcua las coodenadas de una linea
 import funciones
+
 def main():
     m=2
     b=3
-    x=5
-    y = funciones.calcular_y(x, m, b)
-    print(f'Para x={x}, y ={y}')
+    X= [x for x in range(1,11)]
+    Y = [funciones.calcular_y(x, m, b) for x in X]
+    print("Enteros")
+    coordenadas_enteros = list(zip(X,Y))
+    print(coordenadas_enteros)
 
 if __name__ == '__main__':
     main()
