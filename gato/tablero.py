81d2bcb287042ee59828cfa38a609ba903be4b54
santiagohernandezalicia <nolooolvides@gmail>
2025-02-04 10:51:44 -0700

    agregamos dos funciones mas juego y ganador

--- gato/tablero.py
+++ gato/tablero.py
@@ -39,17 +39,72 @@ def usuario(simbolos:dict):
         else:
             print("Numero incorrecto")
 
+def juego(simbolos:dict):
+    '''juego'''
+    lista_combinaciones=[
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9'],
+        ['1','5','9'],
+        ['3','5','7'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['3','6','9']
+    ]
+
+    en_juego=True
+    gandor =""
+    movimientos = 0
+    dibuja_tablero(simbolos)
+    while en_juego:
+        if movimientos <=9:
+            usuario(simbolos)
+        dibuja_tablero(simbolos)
+        movimientos +=1
+        gana = checa_winner(simbolos, lista_combinaciones)
+        if gana is True:
+            en_juego = False
+            ganador = 'Usuario'
+        ia(simbolos)
+        dibuja_tablero(simbolos)
+        movimientos +=1
+        gana = checa_winner(simbolos, lista_combinaciones)
+        if gana is True:
+            en_juego = False
+            ganador = 'Computadora'
+        if movimientos =>9:
+            en_juego = False
+        else:
+            print('Empate')
+
+
+
+
+
+
+def checa_winner(simbolos:dict, combinaciones:list):
+    '''checa ganador'''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]]
+    return None
+
+
 
 if __name__ == '__main__':
     numeros = {str(x) for x in range(1,10)}
     dsimbolos = {x:x for x in numeros}
+    juego(dsimbolos)
+
+
+  ''' 
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
     dibuja_tablero(dsimbolos)
     usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
 
-    ''' 
+  
     x = random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x] = 'X'
