7f8b2f1ee3b03f5055ac44d8d200333334086e4a
santiagohernandezalicia <nolooolvides@gmail>
2025-02-11 11:10:59 -0700

    se depliega plantilla y se lee un txt

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -3,11 +3,28 @@
 def carga_archivo_texto(archivo:str)->list:
     '''carga el archivo de texto y devuelve
      una lista con las oraciones del archivo'''
-    with open(archivo, "r") as file:
+    with open(archivo, 'r', encoding='utf-8') as file:
         oraciones = file.readlines()
     return oraciones
 
+def carga_plantillas(nombre_plantilla)->dict:
+    ''''carga plantiilas del juego a partir de un archivo de texto'''
+    plantillas={}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''despliega la plantilla del juego'''
+    if nivel <=5:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
 if __name__ == "__main__":
-    lista = carga_archivo_texto('./platillas/plantillas-0.txt')
-    for elmento in lista:
-        print(elmento)
+    plantilla = carga_plantillas('plantilla')
+    despliega_plantilla(plantilla,5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    print(lista_oraciones[110:115])
+    texto ="".join(lista_oraciones[110:])
+    print(texto[:100])
