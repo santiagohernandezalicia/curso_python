c7c39bf493d6c7dd26fb8f17547ae742e8acdc7d
santiagohernandezalicia <nolooolvides@gmail>
2025-02-12 11:08:06 -0700

    nuevo obten_palabras y adivina_letra

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -1,4 +1,7 @@
 '''funciones auxiliares'''
+import string
+import unicodedata 
+from random import choice
 
 def carga_archivo_texto(archivo:str)->list:
     '''carga el archivo de texto y devuelve
@@ -21,10 +24,63 @@ def despliega_plantilla(diccionario:dict, nivel:int):
         for renglon in template:
             print(renglon)
 
+def obten_palabras(lista:list)->list:
+    texto =' '.join(lista[120])
+    palabras = texto.split()
+    #convertimos a minusculas
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    #removemos signos de puntuacion y caracteres especiales
+    set_palabras ={palabra.strip(string.punctuation) for palabra in 
+    set_palabras}
+    #removemos numeros, parentesis, corchetes y otros caracteres
+    set_palabras = {palabra for palabra in set_palabras if palabra.isalpha()}
+    #remover acentos
+    set_palabras = {unicodedata.normalize('NFKD', palabra).encode
+    ('ascii','ignore').decode('ascii') for palabra in set_palabras}
+    return list(set_palabras)
+
+def adivina_letra(abc:dict, palabra:str, letras_adivinas:set,turnos:int):
+    '''adivina una letra de la palabra'''
+    palabra_oculta = ""
+    for letra in palabra:
+        if letra in letras_adivinas:
+            palabra_oculta += letra
+        else:
+            palabra_oculta += '_'
+    print(f'tienes {turnos} turnos')
+    print(f'palabra: {palabra_oculta}')
+    print(f'el abecedario es: {abc}')
+    letra = input('ingresa una letra: ')
+    letra = letra.lower()
+    if len(letra) != 1 or letra not in abc:
+        print('ingresa una letra valida')
+    else:
+        if abc[letra] == "*":
+            print('ya habias ingresado esta letra')
+        if letra in palabra:
+            letras_adivinas.add(letra)
+        else:
+            turnos -= 1
+
+
+
+
+
 if __name__ == "__main__":
     plantilla = carga_plantillas('plantilla')
     despliega_plantilla(plantilla,5)
     lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
-    print(lista_oraciones[110:115])
-    texto ="".join(lista_oraciones[110:])
-    print(texto[:100])
+    lista_palabras = obten_palabras(lista_oraciones)
+    print(len(lista_palabras))
+    p = choice(lista_palabras)
+    print(p)    
+    abcdario = {letra:letra for letra in string.ascii_lowercase}
+    letras_adivinas = set()
+    turnos = 5
+    print(f"tienes {turnos} turnos")
+    adivina_letra(abcdario, p, letras_adivinas, turnos)
+    print(f"tienes {turnos} turnos")
+    adivina_letra(abcdario, p, letras_adivinas, turnos)
+
+    
